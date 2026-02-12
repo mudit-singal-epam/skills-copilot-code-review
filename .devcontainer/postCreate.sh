0598b74c9ab72ae429ce4189b55c6908e623b16f
Mudit Singal <mudit_singal@epam.com>
2026-02-12 10:37:02 +0000

    Refactor CSS styles: Update color palette, improve layout structure, and enhance button styles for better responsiveness and user experience.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,6 @@
 # Prepare python environment
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Prepare MongoDB Development DB
 ./.devcontainer/installMongoDB.sh
